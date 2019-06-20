d59d04899db765aebd91c1782ba9f7e9d9218f1f
龚幸昌 <wb-gxc396570@alibaba-inc.com>
2019-06-20 17:36:27 +0800

    add Boolean1.0

--- src/Boolean.java
+++ src/Boolean.java
@@ -1,6 +1,9 @@
 public class Boolean {
     public static void main(String[] args) {
         System.out.println("学习布尔运算");
+        int age = 5;
+        boolean isBoolean = age >=6 && age <= 12;
+        System.out.println(isBoolean? "yes": "no");
 
     }
 }
