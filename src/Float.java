bfcab0f89490aaecce480b0e64c0c357a09dce8f
龚幸昌 <wb-gxc396570@alibaba-inc.com>
2019-06-20 15:36:23 +0800

    add 4.1

--- src/Float.java
+++ src/Float.java
@@ -1,5 +1,12 @@
 public class Float {
     public static void main(String[] args) {
+        System.out.println("学习浮点数运算");
+        double x = 1.0 / 10;
+        double y = 1 - 9.0 / 10;
+        // 观察x和y是否相等:
+        System.out.println(x);
+        System.out.println(y);
+
 
     }
 }
